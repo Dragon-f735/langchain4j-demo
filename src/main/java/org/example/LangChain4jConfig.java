51c890d9cf2673aad7d555fb5e607d59dd5a9524
Dragon <zhanglong197@163.com>
2026-06-13 09:44:29 +0800

    20260613

--- src/main/java/org/example/LangChain4jConfig.java
+++ src/main/java/org/example/LangChain4jConfig.java
@@ -1,19 +1,40 @@
 package org.example;
 
 import com.sun.org.apache.regexp.internal.RE;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentLoader;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.source.FileSystemSource;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.chroma.ChromaEmbeddingStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.DefaultDocumentLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.time.Duration;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Configuration
 public class LangChain4jConfig {
@@ -53,6 +74,28 @@ public class LangChain4jConfig {
                 .build();
     }
 
+    @Bean
+    public EmbeddingModel embeddingModel () {
+        return OpenAiEmbeddingModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey("not-needed-for-ollama")
+                .modelName("nomic-embed-text:v1.5")
+                .timeout(Duration.ofSeconds(60))
+                .logRequests(true)
+                .logResponses(true)
+                .build();
+    }
+
+    @Bean
+    public EmbeddingStore embeddingStore (EmbeddingModel embeddingModel) {
+
+        return ChromaEmbeddingStore.builder()
+                .baseUrl("http://127.0.0.1:8000")
+                .collectionName("langchain4j-chroma-vector")
+                .timeout(Duration.ofSeconds(60))
+                .build();
+    }
+
     /**
      * 2. 手动创建你的智能体（AiService） Bean
      * 这样你在业务代码里就能直接 @Autowired QAQueryAgent 了
@@ -61,12 +104,14 @@ public class LangChain4jConfig {
     public QAQueryAgent qaQueryAgent(ChatLanguageModel chatLanguageModel,
                                      StreamingChatLanguageModel streamingChatLanguageModel,
                                      ChatMemory chatMemory,
-                                     ChatMemoryProvider chatMemoryProvider) {
+                                     ChatMemoryProvider chatMemoryProvider,
+                                     ContentRetriever contentRetriever) {
         return AiServices.builder(QAQueryAgent.class)
                 .chatLanguageModel(chatLanguageModel)
                 .streamingChatLanguageModel(streamingChatLanguageModel)
                 //.chatMemory(chatMemory)
                 .chatMemoryProvider(chatMemoryProvider)
+                .contentRetriever(contentRetriever)
                 // 后续如果你加了别的组的知识库工具，或者你自己的向量库，直接在下面 .tools() 即可
                 // .contentRetriever(yourContentRetriever)
                 .build();
@@ -93,6 +138,37 @@ public class LangChain4jConfig {
                         .build();
             }
         };
+    }
+
+    //@Bean
+    public EmbeddingStore store(EmbeddingModel embeddingModel,
+                                EmbeddingStore embeddingStore) throws IOException {
+
+        Document document = FileSystemDocumentLoader.loadDocument(Paths.get("E:\\AI_PROJECT\\langchain4j-demo\\src\\main\\resources\\Git教程.md"), new ApacheTikaDocumentParser());
+
+        //InMemoryEmbeddingStore inMemoryEmbeddingStore = new InMemoryEmbeddingStore<>();
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(500, 100);
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingStore(embeddingStore)
+                .documentSplitter(splitter)
+                .embeddingModel(embeddingModel)
+                .build();
+        ingestor.ingest(document);
+        return embeddingStore;
+    }
+
+    @Bean
+    public ContentRetriever contentRetriever(EmbeddingStore embeddingStore,
+                                             EmbeddingModel embeddingModel) {
+
+       return EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+               .embeddingModel(embeddingModel)
+                .minScore(0.5)
+                .maxResults(3)
+                .build();
     }
 
 }
--- src/main/java/org/example/QAQueryAgent.java
+++ src/main/java/org/example/QAQueryAgent.java
@@ -9,10 +9,11 @@ import reactor.core.publisher.Flux;
 
 public interface QAQueryAgent {
 
-    /*@SystemMessage({
+    @SystemMessage({
             "你是一个资深的 Java 后端专家和架构师。",
-            "请用专业、严谨且通俗易懂的语言来回答研发人员提出的技术问题。"
-    })*/
-    @SystemMessage(fromResource = "system.txt")
+            "请用专业、严谨且通俗易懂的语言来回答研发人员提出的技术问题。",
+            "必须用中文回复"
+    })
+    //@SystemMessage(fromResource = "system.txt")
     TokenStream chat(@UserMessage String userMessage, @MemoryId String memoryId); // 声明式接口，方法名叫 chat 或 ask 都可以
 }
